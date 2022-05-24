830b211b3d3c02b45934af6edd85fc81f85cafbd
Chris-Durr <chrisdurr88@gmail.com>
2022-05-24 16:25:17 +0000

    poco falta

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Fav_people, Fav_planets
 #from models import Person
 
 app = Flask(__name__)
@@ -31,13 +31,77 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+def get_user():
+    alluser = User.query.all() 
+    alluser = list(map(lambda elemento: elemento.serialize(),
+    alluser)) 
+    print(alluser)
+    return jsonify({"resultado":alluser})
 
     return jsonify(response_body), 200
+# Aqui uestras rutas
+@app.route('/people', methods=['GET'])
+def get_people():
+    allpeople = People.query.all()  #retorna un arreglo de clases
+    allpeople = list(map(lambda elemento: elemento.serialize(),
+    allpeople)) # iteron en cada clase y almaceno el resultado de la funcion serialize
+    print(allpeople)
+    return jsonify({"resultado":allpeople})
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    allplanets = Planets.query.all() 
+    allplanets = list(map(lambda elemento: elemento.serialize(),
+    allplanets)) 
+    print(allplanets)
+    return jsonify({"resultado":allplanets})
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_one_people(id):
+    #bajo un parametro especifico
+    #onepeople = People.query.filter_by(id=id).first()
+    #buscar solo por ID
+    onepeople = People.query.get(id)
+    if onepeople:
+        onepeople = onepeople.serialize()
+        return jsonify({"resultado": onepeople})
+    else:
+        return jsonify({"resultado": "Try harder, young padawan"})
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_one_planet(id):
+    oneplanet = Planets.query.get(id)
+    if oneplanet:
+        oneplanet = oneplanet.serialize()
+        return jsonify({"resultado": oneplanet})
+    else:
+        return jsonify({"resultado": "The planet you're looking for is not in the archive"})
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_fav_people(people_id):
+    onepeople = People.query.get(people_id)
+    if onepeople:
+        new = Fav_people()
+        new.user_id = 1
+        new.people_id = people_id
+        db.session.add(new) #agregar rejistro a la Base de Datos
+        db.session.commit() #guardar los cambios
+        return jsonify({"resultado": "Success! Woho!"})
+    else:
+        return jsonify({"resultado": "Try harder, young padawan"})
+
+@app.route('/favorite/planets/<int:planets_id>', methods=['POST'])
+def add_fav_planets(planets_id):
+    oneplanet = Planets.query.get(planets_id)
+    if oneplanet:
+        new = Fav_planets()
+        new.user_id = 1
+        new.planets_id = planets_id
+        db.session.add(new) #agregar rejistro a la Base de Datos
+        db.session.commit() #guardar los cambios
+        return jsonify({"resultado": "Success! Woho!"})
+    else:
+        return jsonify({"resultado": "Try harder, young padawan"})
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
